e7c3361beb4a63a74c13f675aa286ad80d91c61a
mat_abgaryan <matabgaryan1@gmail.com>
2022-03-07 15:05:34 +0400

    Fix: Code Clean up

--- my-app/src/redux/actions/actions.types.ts
+++ my-app/src/redux/actions/actions.types.ts
@@ -2,8 +2,4 @@ export const ADD_TODO: string = "ADD_TODO";
 export const CLEAR_TODO_LIST: string = "CLEAR_TODO_LIST";
 export const DELETE_TODO: string = "DELETE_TODO";
 export const TOGGLE_TODO: string = "TOGGLE_TODO";
-export const TOGGLE_TODO1: string = "TOGGLE_TODO1";
-export const TOGGLE_TODO2: string = "TOGGLE_TODO2";
-export const TOGGLE_TODO3: string = "TOGGLE_TODO3";
-export const TOGGLE_TODO4: string = "TOGGLE_TODO4";
-export const TOGGLE_TODO5: string = "TOGGLE_TODO5";
+
