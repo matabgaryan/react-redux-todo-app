ec8e24011f2cef0014e3ecd8e9e333914b424261
mat_abgaryan <matabgaryan1@gmail.com>
2022-03-07 15:02:10 +0400

    Fix: Commite test branch 2

--- my-app/src/redux/actions/actions.types.ts
+++ my-app/src/redux/actions/actions.types.ts
@@ -4,3 +4,6 @@ export const DELETE_TODO: string = "DELETE_TODO";
 export const TOGGLE_TODO: string = "TOGGLE_TODO";
 export const TOGGLE_TODO1: string = "TOGGLE_TODO1";
 export const TOGGLE_TODO2: string = "TOGGLE_TODO2";
+export const TOGGLE_TODO3: string = "TOGGLE_TODO3";
+export const TOGGLE_TODO4: string = "TOGGLE_TODO4";
+export const TOGGLE_TODO5: string = "TOGGLE_TODO5";
