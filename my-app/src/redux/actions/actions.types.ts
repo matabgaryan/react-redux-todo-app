c8eedc51a17f387a89b8bf4989fd76c75bc1bc3b
mat_abgaryan <matabgaryan1@gmail.com>
2022-03-07 15:00:26 +0400

    Fix: Commite dev 2

--- my-app/src/redux/actions/actions.types.ts
+++ my-app/src/redux/actions/actions.types.ts
@@ -2,3 +2,5 @@ export const ADD_TODO: string = "ADD_TODO";
 export const CLEAR_TODO_LIST: string = "CLEAR_TODO_LIST";
 export const DELETE_TODO: string = "DELETE_TODO";
 export const TOGGLE_TODO: string = "TOGGLE_TODO";
+export const TOGGLE_TODO1: string = "TOGGLE_TODO1";
+export const TOGGLE_TODO2: string = "TOGGLE_TODO2";
