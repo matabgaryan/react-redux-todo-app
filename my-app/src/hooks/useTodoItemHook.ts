49d56a4f7b36ed802787cef4c6111966afc26e5a
mat_abgaryan <matabgaryan1@gmail.com>
2022-03-07 14:59:55 +0400

    Fix: Commite dev 1

--- my-app/src/hooks/useTodoItemHook.ts
+++ my-app/src/hooks/useTodoItemHook.ts
@@ -1,11 +1,12 @@
 import { useDispatch } from "react-redux";
 import { deleteTodo, toggleTodo } from "../redux/actions";
+import { useState } from "react";
 const colorBackground = '#A9A9A9';
 const colorWhite = '#ffffff';
 
 export const useTodoItemHook = ({ completed } : { completed : boolean}) => {
   const dispatch = useDispatch();
-
+  const [toggle, setToggle] = useState(false)
   const styled = {
     textDecoration: completed ? "line-through" : "none",
     backgroundColor: completed ? colorBackground : colorWhite
@@ -13,15 +14,18 @@ export const useTodoItemHook = ({ completed } : { completed : boolean}) => {
 
   const handleDelete: (id: number) => void = (id) => {
     dispatch(deleteTodo(id));
+    setToggle(false)
   };
 
   const handleToggle: (id: number) => void = (id) => {
     dispatch(toggleTodo(id));
+    setToggle(true)
   };
 
   return {
     handleDelete,
     handleToggle,
-    styled
+    styled,
+    toggle
   }
 }
